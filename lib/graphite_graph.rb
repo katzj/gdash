1574bc4eb751fecb685fb36ea6248c6f0b994653
Jeremy Katz <katzj@hubspot.com>
2011-11-15 20:36:54 -0500

    support :template: to specify the graphite color template to use

--- lib/graphite_graph.rb
+++ lib/graphite_graph.rb
@@ -27,7 +27,8 @@ class GraphiteGraph
                    :ymin => nil,
                    :ymax => nil,
                    :area => :none,
-                   :hide_grid => nil}.merge(@overrides)
+                   :hide_grid => nil,
+                   :template => nil}.merge(@overrides)
 
   end
 
@@ -182,7 +183,7 @@ class GraphiteGraph
     url_parts = []
     colors = []
 
-    [:title, :vtitle, :from, :width, :height, :until].each do |item|
+    [:title, :vtitle, :from, :width, :height, :until, :template].each do |item|
       url_parts << "#{item}=#{properties[item]}" if properties[item]
     end
 
